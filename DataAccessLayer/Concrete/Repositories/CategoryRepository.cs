29ef8548ccc4d6524fabd0038aa60b17d6c42323
Ayhan KARAMAN <websitem28@gmail.com>
2021-08-23 15:54:21 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Repositories/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    class CategoryRepository : ICategoryDal
+    {
+        public void Add(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Category> List()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        Context context = new Context();
+        DbSet<TEntity> _entity;
+        public GenericRepository()
+        {
+            _entity = context.Set<TEntity>();
+        }
+
+        public void Add(TEntity entity)
+        {
+            _entity.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            _entity.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+          return  _entity.Where(filter).SingleOrDefault();
+        }
+
+        public List<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
+        {
+            return filter == null ? _entity.ToList() : _entity.Where(filter).ToList();
+
+        }
+
+        public void Update(TEntity entity)
+        {
+            context.SaveChanges();
+        }
+    }
+}
